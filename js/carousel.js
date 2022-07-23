e82ed176855e0fe1b080ef63b13fb5f1df2cd947
tatianimeneghini <tatiani.meneghini@gmail.com>
2022-07-23 19:08:16 -0300

    cambios del modulo 3

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,37 @@
 var btns = document.querySelectorAll(".listaDeArticulos-slider-item");
+var noticias = document.querySelectorAll(".listaDeArticulos-item");
+
+var new0 = document.querySelector("#new0");
+var new1 = document.querySelector("#new1");
+var new2 = document.querySelector("#new2");
+
+new0.style.display = "block";
+
+var indicadorSlideActual = document.createElement("span");
+indicadorSlideActual.classList.add("ocultarVisualmente");
+indicadorSlideActual.textContent = "Slide actual";
 
 // Percurre todos los botones controladores
 btns.forEach(function (btn) {
   btn.addEventListener("click", function () {
+    noticias.forEach(
+      function (noticia) {
+        noticia.style.display = "none";
+
+        if (
+          this.getAttribute("data-sliderItem") ===
+          noticia.getAttribute("data-noticia")
+        ) {
+          noticia.style.display = "block";
+        }
+      }.bind(this)
+    );
+
+    document
+      .querySelector(".listaDeArticulos-slider-item .ocultaVisualmente")
+      .remove();
+    this.append(indicadorSlideActual);
+
     // Quita clase 'activo' de otros botones
     btns.forEach(function (btnRemoveClass) {
       btnRemoveClass.classList.remove("listaDeArticulos-slider-item--activo");
